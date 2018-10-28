093e0716270f38e17c27074909fbba671ebbd0f6
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-10-28 05:08:17 +0000

    # CINERINO-20 ### Changed - タスクサービスのエラーハンドリングを調整。
    # CINERINO-22
    ### Changed
    - 決済承認インターフェースを拡張。

--- dist/client/development/main.js
+++ dist/client/development/main.js
@@ -1,9 +1,9 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([["main"],{
 
 /***/ "../../node_modules/moment/locale sync recursive ^\\.\\/.*$":
-/*!******************************************************************************************!*\
-  !*** /Users/phi.nt/source/toei-ticket/frontend/node_modules/moment/locale sync ^\.\/.*$ ***!
-  \******************************************************************************************/
+/*!***********************************************************************************!*\
+  !*** C:/projects/toei-cinerino-frontend/node_modules/moment/locale sync ^\.\/.*$ ***!
+  \***********************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -9366,7 +9366,7 @@ _angular_platform_browser__WEBPACK_IMPORTED_MODULE_4__["platformBrowser"]().boot
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/phi.nt/source/toei-ticket/frontend/src/client/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! C:\projects\toei-cinerino-frontend\src\client\src\main.ts */"./src/main.ts");
 
 
 /***/ }),
--- dist/server/app/forms/Purchase/Mvtk/MvtkInputForm.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * ムビチケ認証
- */
-exports.default = (req) => {
-    req.checkBody('mvtk', `${req.__('common.mvtk')}${req.__('common.validation.required')}`).notEmpty();
-    req.checkBody('mvtk', `${req.__('common.mvtk')}${req.__('common.validation.is_json')}`).isJSON();
-};

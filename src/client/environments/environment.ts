221fa97eed5ee55591760454fd30377608eadbdd
Akito Hataguchi <hataguchi@motionpicture.jp>
2018-11-30 19:01:33 +0900

    TOEI-41 メール文言修正

--- src/client/environments/environment.ts
+++ src/client/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     APP_PREFIX: 'TO',
     PROJECT_ID: 'toei-production',
-    SITE_URL: 'https://toei-frontend-production.azurewebsites.net',
+    SITE_URL: 'https://ticket.toeitheaters.com',
     PORTAL_SITE_URL: 'http://theaters.toei.co.jp',
     WAITER_SERVER_URL: 'https://waiter-production.appspot.com',
 

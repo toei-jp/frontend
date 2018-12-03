868a759074aad370af94c5dc57b7117375292c6a
Akito Hataguchi <hataguchi@motionpicture.jp>
2018-12-03 18:42:02 +0900

    TOEI-41 注意事項修正

--- src/client/app/components/parts/purchase-note/purchase-note.component.ts
+++ src/client/app/components/parts/purchase-note/purchase-note.component.ts
@@ -1,10 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 const PURCHASE_NOTE = {
-    '01': { ordinance: '東京都青少年健全育成条例', limit: '23:00' },
-    '12': { ordinance: '徳島県青少年健全育成条例', limit: '23:00' },
-    '18': { ordinance: '鹿児島県青少年保護育成条例', limit: '23:00' },
-    '19': { ordinance: '千葉県青少年健全育成条例', limit: '23:00' }
+    '01': { ordinance: '東京都青少年の健全な育成に関する条例', limit: '23:00' }
 };
 
 @Component({
@@ -22,10 +19,7 @@ export class PurchaseNoteComponent implements OnInit {
     public ngOnInit() {
         this.ordinance = '';
         this.limit = '';
-        if (this.theaterCode === '01'
-            || this.theaterCode === '12'
-            || this.theaterCode === '18'
-            || this.theaterCode === '19') {
+        if (this.theaterCode === '01') {
             this.ordinance = PURCHASE_NOTE[this.theaterCode].ordinance;
             this.limit = PURCHASE_NOTE[this.theaterCode].limit;
         }
--- src/client/app/components/parts/purchase-term/purchase-term.component.ts
+++ src/client/app/components/parts/purchase-term/purchase-term.component.ts
@@ -1,10 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 const PURCHASE_TERM = {
-    '01': { ordinance: '東京都青少年健全育成条例', limit: '23:00' },
-    '12': { ordinance: '徳島県青少年健全育成条例', limit: '23:00' },
-    '18': { ordinance: '鹿児島県青少年保護育成条例', limit: '23:00' },
-    '19': { ordinance: '千葉県青少年健全育成条例', limit: '23:00' }
+    '01': { ordinance: '東京都青少年健全育成条例', limit: '23:00' }
 };
 
 @Component({
@@ -21,10 +18,7 @@ export class PurchaseTermComponent implements OnInit {
     public ngOnInit() {
         this.ordinance = '';
         this.limit = '';
-        if (this.theaterCode === '01'
-            || this.theaterCode === '12'
-            || this.theaterCode === '18'
-            || this.theaterCode === '19') {
+        if (this.theaterCode === '01') {
             this.ordinance = PURCHASE_TERM[this.theaterCode].ordinance;
             this.limit = PURCHASE_TERM[this.theaterCode].limit;
         }

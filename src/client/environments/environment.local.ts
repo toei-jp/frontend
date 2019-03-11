eca6558b8e305931471826b44dce98b6e3a95300
Akito Hataguchi <hataguchi@motionpicture.jp>
2019-03-11 16:32:30 +0900

    ローカル用設定ファイル変更

--- src/client/environments/environment.local.ts
+++ src/client/environments/environment.local.ts
@@ -4,10 +4,10 @@ export const environment = {
     PROJECT_ID: 'toei-development',
     SITE_URL: 'https://toei-frontend-development.azurewebsites.net',
     PORTAL_SITE_URL: '/#/purchase/schedule',
-    WAITER_SERVER_URL: '',
-    CINERINO_API_ENDPOINT: 'https://toei-cinerino-api-test.azurewebsites.net',
-    // WAITER_SERVER_URL: 'https://waiter-development.appspot.com',
-    // CINERINO_API_ENDPOINT: 'https://toei-cinerino-api-development.azurewebsites.net',
+    // WAITER_SERVER_URL: '',
+    // CINERINO_API_ENDPOINT: 'https://toei-cinerino-api-test.azurewebsites.net',
+    WAITER_SERVER_URL: 'https://waiter-development.appspot.com',
+    CINERINO_API_ENDPOINT: 'https://toei-cinerino-api-development.azurewebsites.net',
     ANALYTICS_ID: '',
 
     TRANSACTION_TIME: '120'
